9e9e1df28bcdebaabf4f2578a2a7b5b5e049c667
hemingus <heming.hanevik@gmail.com>
2023-01-15 13:37:00 +0100

    Connection to cosmosDB

---
+++ src/Controllers/CommentController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using web_backend.Models;
+
+namespace web_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] 
+    public class CommentController : ControllerBase
+    {
+        
+
+        private IEnumerable<CommentDto> generateComments()
+        {
+            List<CommentDto> comments = new List<CommentDto>();
+            comments.Add(new CommentDto(1, "Alf", "Hallais!"));
+            comments.Add(new CommentDto(2, "Gunn", "Heisann!"));
+
+            return comments;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<CommentDto>> getAllComments()
+        {
+            var settings = System.Configuration.ConfigurationManager.AppSettings;
+            //return generateComments();
+            using CosmosClient client = 
+                new(accountEndpoint: settings["CosmosDbUrl"],
+                authKeyOrResourceToken: settings["CosmosDbSecret"]
+                );
+
+            var container = client.GetContainer(
+                settings["CosmosDatabaseName"], 
+                settings["CosmosContainerName"]
+                );
+
+            using FeedIterator<CommentDto> feed = container.GetItemQueryIterator<CommentDto>(
+                queryText: "SELECT * FROM comments WHERE comments.name = 'Severin'"
+                );
+
+            while (feed.HasMoreResults)
+            {
+                FeedResponse<CommentDto> response = await feed.ReadNextAsync();
+
+                return response;
+
+            }
+            return null; 
+
+        }
+    }
+}
---
+++ src/Models/CommentDto.cs
@@ -0,0 +1,16 @@
+﻿namespace web_backend.Models
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CommentBody { get; set; }
+
+        public CommentDto(int id, string name, string commentBody)
+        {
+            Id = id;
+            Name = name;
+            CommentBody = commentBody;
+        }
+    }
+}
