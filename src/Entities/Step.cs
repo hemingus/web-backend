ed6d594803135349cd7ee5c0270a0176bbd78d13
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-12-11 23:42:12 +0100

    Added new entities

---
+++ src/Entities/Step.cs
@@ -0,0 +1,19 @@
+﻿namespace web_backend.Entities
+{
+    public class Step
+    {
+        public string StepId { get; set; }
+        public string Description { get; set; }
+        public Boolean IsComplete { get; set; }
+        public string TimeCreated { get; set; }
+
+        public Step(string description)
+        {
+            StepId = Guid.NewGuid().ToString();
+            Description = description;
+            IsComplete = false;
+            TimeCreated = DateTime.Now.ToString();
+
+        }
+    }
+}
---
+++ src/Entities/TaskEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace web_backend.Entities
+{
+    public class TaskEntity
+    {
+        public string TaskId { get; set; }
+        public string Description { get; set; }
+        public Boolean IsComplete { get; set; }
+        public ICollection<Subtask> Subtasks { get; set; }
+        public string Created { get; set; }
+
+        public TaskEntity(string description, Boolean isComplete)
+        {
+            Description = description;
+            IsComplete = isComplete;
+            Subtasks = new List<Subtask>();
+            TaskId = Guid.NewGuid().ToString();
+            Created = DateTime.Now.ToString();
+        }
+
+    }
+}
