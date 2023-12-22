267eb4ad3330cdaeee18b527a61737de44bc80a0
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-12-22 22:53:15 +0100

    Fixed CosmosContext

--- src/DbContexts/CosmosContext.cs
+++ src/DbContexts/CosmosContext.cs
@@ -22,16 +22,18 @@ namespace web_backend.DbContexts
                 .HasData(
                     new Comment("Ask", "Ka e det der igjen?"));
 
-            modelBuilder.HasDefaultContainer("Comment").HasManualThroughput(400);
+            //modelBuilder.HasDefaultContainer("Comment").HasManualThroughput(400);
             modelBuilder.Entity<Comment>().HasKey(c => c.Id);
+            modelBuilder.Entity<Comment>().ToContainer("Comment");
             modelBuilder.Entity<Comment>().HasPartitionKey(c => c.PartitionKey);
 
             // Tasks
             modelBuilder.Entity<TaskEntity>()
                 .HasData(
                     new TaskEntity("My first task from DB", false));
-            modelBuilder.HasDefaultContainer("Tasks").HasManualThroughput(400);
+            modelBuilder.HasDefaultContainer("TaskEntity").HasManualThroughput(400);
             modelBuilder.Entity<TaskEntity>().HasKey(t => t.Id);
+            modelBuilder.Entity<TaskEntity>().ToContainer("TaskEntity");
             modelBuilder.Entity<TaskEntity>().HasPartitionKey(t => t.PartitionKey);
 
             base.OnModelCreating(modelBuilder);
