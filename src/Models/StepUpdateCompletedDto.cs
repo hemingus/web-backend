7476095f6fb20a026b71185f893301c498091e95
Heming Hanevik <Heming.Hanevik@student.uib.no>
2024-01-11 16:31:49 +0100

    Added Dtos & controller method for updating status of subtask and step

---
+++ src/Models/StepUpdateCompletedDto.cs
@@ -0,0 +1,11 @@
+﻿namespace web_backend.Models
+{
+    public class StepUpdateCompletedDto
+    {
+        public bool IsComplete { get; set; }
+        public StepUpdateCompletedDto(bool isComplete)
+        {
+            IsComplete = isComplete;
+        }
+    }
+}
