c312acee9dc13a452a307bdf9c2fed4db0432a35
hemingus <54212159+hemingus@users.noreply.github.com>
2024-02-18 19:31:13 +0100

    Revert "Added DataAnnotations on entities"

--- src/Entities/Subtask.cs
+++ src/Entities/Subtask.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace web_backend.Entities
+﻿namespace web_backend.Entities
 {
     public class Subtask
     {
-        [Key]
         public string TaskId { get; set; }
         public string Id { get; set; }
-        [Required]
-        [MaxLength(200)]
         public string Description { get; set; }
         public Boolean IsComplete { get; set; }
-        [DataType(DataType.DateTime)]
         public string Timestamp { get; set; }
         public int Order { get; set; }
         public ICollection<Step> Steps { get; set; }
