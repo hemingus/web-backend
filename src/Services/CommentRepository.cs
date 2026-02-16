34d8ebc42616a5cce888162b6cdd164296c967b7
hemingus <heming.hanevik@gmail.com>
2026-02-16 15:19:25 +0100

    add CommentRepository and and interface

---
+++ src/Services/CommentRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using web_backend.DbContexts;
+using web_backend.Entities;
+
+namespace web_backend.Services
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly CosmosContext _context;
+        public CommentRepository(CosmosContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            //_context.Database.EnsureDeleted();
+            //_context.Database.EnsureCreated();
+        }
+        public async Task<Comment> GetCommentByIdAsync(string commentId)
+        {
+            try
+            {
+                Comment comment = await _context.Comments.SingleOrDefaultAsync(c => c.Id == commentId);
+                return comment;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public void AddComment(Comment comment)
+        {
+            _context.Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            _context.Comments.Remove(comment);
+        }
+    }
+}
---
+++ src/Services/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using web_backend.Entities;
+
+namespace web_backend.Services
+{
+    public interface ICommentRepository
+
+    {
+        // Comment
+        Task<IEnumerable<Comment>> GetCommentsAsync();
+        Task<Comment> GetCommentByIdAsync(string commentId);
+        void AddComment(Comment comment);
+        void RemoveComment(Comment comment);
+
+        // Save changes
+        Task<bool> SaveChangesAsync();
+    }
+}
