d77d34e529b2ea5a6073983c7795c94e43ff3294
hemingus <heming.hanevik@gmail.com>
2026-02-16 15:12:57 +0100

    update TaskEntity

--- src/Entities/TaskEntity.cs
+++ src/Entities/TaskEntity.cs
@@ -1,17 +1,24 @@
-﻿namespace web_backend.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using web_backend.Models;
+
+namespace web_backend.Entities
 {
     public class TaskEntity
     {
         public string Id { get; set; }
+        [Required, MaxLength(500)]
         public string Description { get; set; }
         public Boolean IsComplete { get; set; }
         public ICollection<Subtask> Subtasks { get; set; }
         public string Timestamp { get; set; }
         public int Order { get; set; }
-        public string PartitionKey { get; set; } = "2";
+        
 
-        // New: owner identifier to associate tasks with a user
+        [Required]
         public string OwnerId { get; set; } = string.Empty;
+        [Required]
+        public string ProjectId { get; set; } = string.Empty;
+        public string PartitionKey => OwnerId;
 
         public TaskEntity(string description, Boolean isComplete, int order)
         {
