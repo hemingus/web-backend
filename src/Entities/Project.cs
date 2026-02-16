fed2e809afec9e75b6293245b96d754e9407c984
hemingus <heming.hanevik@gmail.com>
2026-02-16 15:12:42 +0100

    add Project entity

---
+++ src/Entities/Project.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_backend.Entities
+{
+    public class Project
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+
+        public Project(string name, string? description)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Description = description;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
