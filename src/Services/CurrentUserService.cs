5312d9c81578d7828fd2c85f90630c3a72015bb6
hemingus <heming.hanevik@gmail.com>
2026-02-19 15:08:12 +0100

    add Service , CurrentUserService for authentication/authorization

---
+++ src/Services/CurrentUserService.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace web_backend.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string UserId =>
+            _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+            ?? _httpContextAccessor.HttpContext?.User?.FindFirst("sub")?.Value
+            ?? string.Empty;
+
+        public bool IsAuthenticated =>
+            _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+    }
+}
---
+++ src/Services/ICurrentUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace web_backend.Services
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+        bool IsAuthenticated { get; }
+    }
+}
