f2f028e2320e69d8b13fab18777ab4ca05184887
hemingus <heming.hanevik@gmail.com>
2026-02-27 14:56:48 +0100

    Add Project Dtos to DTOs

---
+++ src/Models/DTOs/ProjectDto.cs
@@ -0,0 +1,18 @@
+﻿namespace web_backend.Models.DTOs
+{
+    public class ProjectDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public ProjectDto(string id, string name, string? description, DateTimeOffset createdAt)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ src/Models/DTOs/ProjectForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace web_backend.Models.DTOs
+{
+    public class ProjectForCreationDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = "Focus Dirctor Project";
+    }
+}
