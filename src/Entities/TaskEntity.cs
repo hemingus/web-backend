f9ab87aa8fc3a49847550917e16cc0f05796c10a
hemingus <heming.hanevik@gmail.com>
2026-03-02 14:47:03 +0100

    update task entity where ownerId no longer in constructor, but added by repo

--- src/Entities/TaskEntity.cs
+++ src/Entities/TaskEntity.cs
@@ -7,15 +7,14 @@ public class TaskEntity
     public string Description { get; set; }
     public bool IsComplete { get; set; } = false;
     public int Order { get; set; }
-    public string OwnerId { get; set; }
+    public string OwnerId { get; set; } = string.Empty;
     public string ProjectId { get; set; }
     public ICollection<Subtask> Subtasks { get; set; } = new List<Subtask>();
 
-    public TaskEntity(string description, int order, string ownerId, string projectId)
+    public TaskEntity(string description, int order, string projectId)
     {
         Description = description;
         Order = order;
-        OwnerId = ownerId;
         ProjectId = projectId;
     }
 }
