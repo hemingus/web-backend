9130cb62b34e76f2a81de8a569882d65df7334ad
Heming Hanevik <heming.hanevik@gmail.com>
2026-03-04 08:26:07 +0100

    update task entity

--- src/Entities/TaskEntity.cs
+++ src/Entities/TaskEntity.cs
@@ -1,20 +1,23 @@
 ﻿using web_backend.Models;
 
-public class TaskEntity
+namespace web_backend.Entities
 {
-    public string Id { get; private set; } = Guid.NewGuid().ToString();
-    public DateTimeOffset Timestamp { get; private set; } = DateTime.UtcNow;
-    public string Description { get; set; }
-    public bool IsComplete { get; set; } = false;
-    public int Order { get; set; }
-    public string OwnerId { get; set; } = string.Empty;
-    public string ProjectId { get; set; }
-    public ICollection<Subtask> Subtasks { get; set; } = new List<Subtask>();
-
-    public TaskEntity(string description, int order, string projectId)
+    public class TaskEntity
     {
-        Description = description;
-        Order = order;
-        ProjectId = projectId;
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset Timestamp { get; private set; } = DateTime.UtcNow;
+        public string Description { get; set; }
+        public bool IsComplete { get; set; } = false;
+        public int Order { get; set; }
+        public string OwnerId { get; set; } = string.Empty;
+        public string ProjectId { get; set; }
+        public ICollection<Subtask> Subtasks { get; set; } = new List<Subtask>();
+
+        public TaskEntity(string description, int order, string projectId)
+        {
+            Description = description;
+            Order = order;
+            ProjectId = projectId;
+        }
     }
 }
