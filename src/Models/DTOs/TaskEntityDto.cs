aaa6462ee2d2615eb07457f684266925deaf331a
Heming Hanevik <heming.hanevik@gmail.com>
2026-03-09 09:18:30 +0100

    Add ProjectId field to TaskEntityDto

--- src/Models/DTOs/TaskEntityDto.cs
+++ src/Models/DTOs/TaskEntityDto.cs
@@ -3,15 +3,17 @@
     public class TaskEntityDto
     {
         public string Id { get; set; }
+        public string ProjectId { get; set; }
         public string Description { get; set; }
         public Boolean IsComplete { get; set; }
         public DateTimeOffset Timestamp { get; set; }
         public int Order { get; set; }
         public IEnumerable<Subtask> Subtasks { get; set; }  
 
-        public TaskEntityDto(string id, DateTimeOffset timestamp, string description, Boolean isComplete, int order, ICollection<Subtask> subtasks)
+        public TaskEntityDto(string id, string projectId, DateTimeOffset timestamp, string description, Boolean isComplete, int order, ICollection<Subtask> subtasks)
         {
             Id = id;
+            ProjectId = projectId;
             Description = description;
             IsComplete = isComplete;
             Timestamp = timestamp;
